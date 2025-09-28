62ba78cd24f80258238d8d74254bb599b40eb336
Sem VB <sem.van.broekhoven@gmail.com>
2025-09-28 22:28:31 +0200

    update workspace reload

--- guitarix/banks/banklist.js
+++ guitarix/banks/banklist.js
@@ -1,5 +1,5 @@
 [
   ["metalpack-v1", "metalpack-v1.gx", 1, 0, [1, 2], 1758536401],
   ["babiskouvalis", "babiskouvalis.gx", 1, 0, [1, 2], 1758536396],
-  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1758741730]
+  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1758897838]
 ]
