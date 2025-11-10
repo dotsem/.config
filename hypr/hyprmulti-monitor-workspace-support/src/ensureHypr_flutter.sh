8bfaf6d937ff922d804d0379b07f26bfbfe55ee2
Sem VB <sem.van.broekhoven@gmail.com>
2025-11-11 00:12:08 +0100

    update hyprflutter to run with nvidia

--- hypr/hyprmulti-monitor-workspace-support/src/ensureHypr_flutter.sh
+++ hypr/hyprmulti-monitor-workspace-support/src/ensureHypr_flutter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pkill hypr_flutter
-pgrep -x "bundle/hypr_flutter" > /dev/null ||  $1 &
+pgrep -x "bundle/hypr_flutter" > /dev/null || __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia $1 &
