5e6b5e8168552523970083450f0c5434c7d6dcd3
Sem VB <sem.van.broekhoven@gmail.com>
2025-11-18 10:39:38 +0100

    update playerctl keybinds

--- hypr/hyprmulti-monitor-workspace-support/src/ensureHypr_flutter.sh
+++ hypr/hyprmulti-monitor-workspace-support/src/ensureHypr_flutter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pkill hypr_flutter
-pgrep -x "bundle/hypr_flutter" > /dev/null || __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia $1 &
+pgrep -x "bundle/hypr_flutter" > /dev/null || GDK_BACKEND=x11 $1 &
